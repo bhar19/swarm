ce351e07c1bf14f220391398210d4cf0feb91512
Bhargava Tadi <tadi.bhargava@gmail.com>
2015-12-11 05:12:48 +0000

    Supress changes for hostconfig

--- cluster/engine.go
+++ cluster/engine.go
@@ -479,7 +479,7 @@ func (e *Engine) UsedMemory() int64 {
 	e.RLock()
 	for _, c := range e.containers {
 		log.WithField("Config values", c.Config).Debugf("Print Config values")
-		log.WithField("Config values", c.HostConfig).Debugf("print HostConfig Values")
+		//log.WithField("Config values", c.HostConfig).Debugf("print HostConfig Values")
 		log.WithField("Config values", c).Debugf("print HostConfig Values")
 		r += c.Config.Memory
 	}
@@ -493,7 +493,7 @@ func (e *Engine) UsedCpus() int64 {
 	e.RLock()
 	for _, c := range e.containers {
 		log.WithField("Config values", c.Config).Debugf("Print Config values")
-		log.WithField("Config values", c.HostConfig).Debugf("print HostConfig Values")
+		//log.WithField("Config values", c.HostConfig).Debugf("print HostConfig Values")
 		log.WithField("Config values", c).Debugf("print HostConfig Values")
 		r += c.Config.CpuShares
 	}
@@ -506,7 +506,8 @@ func (e *Engine) UsedBlkio() int64 {
 	var r int64
 	e.RLock()
 	for _,c := range e.containers {
-		r += c.Config.BlkioWeight
+		r += c.Config.CpuShares
+		//r += c.Config.BlkioWeight
 		//log.WithField("Config values", c.Config).Debugf("Print Config values")
 	}
 	e.RUnlock()
